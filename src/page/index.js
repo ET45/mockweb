fe1869cafc21ca0861274978c18a1c4da499eb01
Emre <emre.tugutlu@gmail.com>
2022-07-04 10:44:31 +0200

    text

--- src/page/index.js
+++ src/page/index.js
@@ -1,6 +1,9 @@
 import "./styles.css";
 
 export default function Page() {
-    return <div class="grid grid-rows=-4 grid-flow-col gap-4"><div><p>{"Stroom & Gas"}</p><img src="image3.jpg" alt="img3" /></div><div><p>Enregieproducten</p><img src="image2.jpg" alt="img2"/></div><div><p>Speciaal voor klanten</p><img src="image1.jpg" alt="img1"/></div></div>
+    return <div class="grid grid-rows=-4 grid-flow-col gap-4">
+        <div><p class="text-center hover:text-left text-white font-bold rounded-t-lg h-16 bg-gradient-to-r from-purple-500 to-purple-500">{"Stroom & Gas"}</p><img src="image3.jpg" alt="img3" /></div>
+        <div><p class="rounded-t-lg h-18 bg-gradient-to-r from-purple-500 to-purple-500">Enregieproducten</p><img src="image2.jpg"  alt="img2"/></div>
+        <div><p class="rounded-t-lg h-18 bg-gradient-to-r from-purple-500 to-purple-500">Speciaal voor klanten</p><img src="image1.jpg" alt="img1"/></div></div>
    
 }
