1ff10e2776c228be536bafab0bffe327e617af1a
Emre <emre.tugutlu@gmail.com>
2022-07-04 09:55:37 +0200

    ..

--- src/page/index.js
+++ src/page/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
 export default function Page() {
-    return <div><img src="image1.jpg" alt="imag"></img></div>
+    return <div className="body"><div><p>{"Stroom & Gas"}</p><img src="image3.jpg" alt="img3" /></div><div><p>Enregieproducten</p><img src="image2.jpg" alt="img2"/></div><div><p>Speciaal voor klanten</p><img src="image1.jpg" alt="img1"/></div></div>
    
 }
