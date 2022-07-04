b02eba83f6a8548fef9886a3f9e8c1b882ab25e2
Emre <emre.tugutlu@gmail.com>
2022-07-04 12:56:52 +0200

    added theme to tailwind

--- src/page/index.js
+++ src/page/index.js
@@ -1,8 +1,8 @@
 import "./styles.css";
 
 export default function Page() {
-    return <div class="grid grid-rows=-4 grid-flow-col gap-4">
-        <div><p class="text-center hover:text-left text-white font-bold rounded-t-lg h-16 bg-gradient-to-r from-purple-500 to-purple-500">{"Stroom & Gas"}</p><img src="image3.jpg" alt="img3" /></div>
+    return <div class="grid grid-rows=-4 grid-flow-col gap-4 font-sans">
+        <div><p class="text-white  rounded-t-2xl h-16 bg-pink-600"><p class="font-bold text-tiny mb-0 my">{"Stroom & Gas"}</p></p><img src="image3.jpg" alt="img3" /></div>
         <div><p class="rounded-t-lg h-18 bg-gradient-to-r from-purple-500 to-purple-500">Enregieproducten</p><img src="image2.jpg"  alt="img2"/></div>
         <div><p class="rounded-t-lg h-18 bg-gradient-to-r from-purple-500 to-purple-500">Speciaal voor klanten</p><img src="image1.jpg" alt="img1"/></div></div>
    
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    fontFamily:{'sans':['sans-serif']},
+    fontSize: {
+      'tiny':'20px',
+    },
     extend: {},
   },
   plugins: [],
