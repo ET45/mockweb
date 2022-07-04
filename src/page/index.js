e82029b4d8926cd8cf90243747097e389d7cd2c6
Emre <emre.tugutlu@gmail.com>
2022-07-04 10:10:43 +0200

    first tailwind

--- src/page/index.js
+++ src/page/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
 export default function Page() {
-    return <div className="body"><div><p>{"Stroom & Gas"}</p><img src="image3.jpg" alt="img3" /></div><div><p>Enregieproducten</p><img src="image2.jpg" alt="img2"/></div><div><p>Speciaal voor klanten</p><img src="image1.jpg" alt="img1"/></div></div>
+    return <div class="grid grid-rows=-4 grid-flow-col gap-4"><div><p>{"Stroom & Gas"}</p><img src="image3.jpg" alt="img3" /></div><div><p>Enregieproducten</p><img src="image2.jpg" alt="img2"/></div><div><p>Speciaal voor klanten</p><img src="image1.jpg" alt="img1"/></div></div>
    
 }
