bee97e7adeaae4981e8b1950973c037e9d2e842e
Emre <E22536@eon.com>
2022-07-06 15:19:02 +0200

    update

--- src/page/index.js
+++ src/page/index.js
@@ -3,9 +3,9 @@ import "./styles.css";
 export default function Page() {
   return (
     <div>
-      <div className=" p-10 px-48 flex w-100 flex-row  ">
+      <div className=" p-10 px-48 flex space-x-4 flex-row pr-64 ">
         <div className="flex w-2/3 relative">
-          <img src="image6.png" alt="img6" width="700" />
+          <img className="h-700" src="image6.png" alt="img6" />
           <div className="absolute">
             <h2 className=" text-left font-bold text-l text-white top-1/8 pr-120 p-4 ">
               BESPAREN OP ENERGIE? MET THUISVOORDEEL WORDT HET ÉXTRA MAKKELIJK.
@@ -27,8 +27,8 @@ export default function Page() {
             </p>
           </div>
         </div>
-        <div className="flex w-1/3">
-          <div className=" border border-black-200 p-4">
+        <div className="flex w-1/3 pl-">
+          <div className=" border border-black-200 p-8">
             <h1 className="text-purple-500 text-lg text-left font-bold">
               Grip op energie
             </h1>
@@ -103,6 +103,99 @@ export default function Page() {
       <div className="flex flex-row place-content-center">
         Onze klanten beoordelen ons met een <strong>8,1</strong>
         <img src="logo.png" alt="logo" />
+      </div>
+      <div className="bg-gray-300 py-4">
+        <h1 className="text-4xl font-bold text-left py-4 pl-52">
+          Vandaag geregeld
+        </h1>
+        <div className="flex flex-row h-6 space-x-16 pr-24 place-content-center pt-4">
+          <div className="icongray">
+            <img className="h-10 pt-2 pl-2" src="Gauge.png" alt="gauge" />
+            <p className="text-tiny pt-3">Meterstanden doorgeven</p>
+          </div>
+          <div className="icongray">
+            <img className="h-10 pt-2 pl-2" src="Graph.png" alt="graph" />
+            <p className="text-tiny pt-3">Mijn verbruik inzien</p>
+          </div>
+          <div className="icongray">
+            <img className="h-10 pt-2 pl-2" src="Money.png" alt="money" />
+            <p className="text-tiny pt-3">Termijnbedrag wijzigen</p>
+          </div>
+          <div className="icongray">
+            <img className="h-10 pt-2 pl-2" src="Paper.png" alt="paper" />
+            <p className="text-tiny pt-3">Facturen bekijken</p>
+          </div>
+        </div>
+        <div className="flex flex-row p-24 place-content-center ">
+          <div className="space-y-8 ">
+            <div className="flex flex-col">
+              <div className="pinkheader">
+                <img className="h-6" src="pink3.png" alt="p3" />
+                <h2>Stroom en Gas</h2>
+              </div>
+              <p>
+                Voor stroom en gas ben je bij ons aan het goede adres. Vul je
+                postcode en huisnummer in de postcodecheck in en je ziet direct
+                ons aanbod.
+              </p>
+              <p>Bekijk Stroom en Gas aanbod</p>
+            </div>
+            <div className="flex flex-col">
+              <div className="pinkheader">
+                <img className="h-6" src="pink2.png" alt="p2" />
+                <h2>Energietarieven</h2>
+              </div>
+              <p>
+                Met de postcodecheck zie je snel en eenvoudig hoe onze
+                energietarieven in jouw situatie uitpakken. Natuurlijk krijg je
+                als nieuwe klant groene stroom bij Essent.
+              </p>
+              <p>Lees meer over onze energietarieven</p>
+            </div>
+            <div className="flex flex-col">
+              <div className="pinkheader">
+                <img className="h-6" src="pink1.png" alt="p1" />
+                <h2>Verhuizen</h2>
+              </div>
+              <p>
+                Bij een verhuizing komt een hoop kijken. Ook het regelen van je
+                energiezaken hoort hierbij. Ga je verhuizen en ben je op zoek
+                naar een energieleverancier? Bekijk dan ons aanbod.
+              </p>
+              <p>Lees meer over verhuizen en energie</p>
+            </div>
+          </div>
+          <div>
+            <div>
+              <h1>Energieleverancier Essent</h1>
+              <p>
+                Als energieleverancier helpen we ruim 2,5 miljoen klanten aan
+                energie in de particuliere en zakelijke markt. We helpen je
+                graag om duurzamer te leven én te besparen. Daar hoort vandaag
+                de dag ook groene stroom bij. Opgewekt met windmolens en
+                zonnepanelen en steeds meer van Nederlandse bodem.
+              </p>
+              <p>
+                Bij Essent kies je uit een aantal standaard stroom- en
+                gasproducten. Welk contract je ook kiest, bij ons zit je goed.
+                Weten wat je bij het huidige tarief per maand gaat betalen? Vul
+                de postcodecheck in en je weet het binnen 1 minuut.
+              </p>
+            </div>
+            <div>
+              <h1>Meer dan alleen een energieleverancier</h1>
+              <p>
+                Essent wil klanten helpen hun huis of zaak comfortabel én
+                energiezuinig te maken. Wij bieden als energiemaatschappij
+                daarom ook andere slimme, energiegerelateerde producten aan.
+                Bijvoorbeeld zonnepanelen om energie te besparen met je eigen
+                dak, verwarming, of isolatie voor een energiezuinige woning.
+                Deze producten worden geleverd door onze servicepartners, van
+                installatie tot en met onderhoud.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,10 @@ module.exports = {
     },
     extend: {
       spacing: {
+        '100': '25rem',
+        '104': '26rem',
+        '108': '27rem',
+        '112': '28rem',
         '120': '30rem',
         '160': '40rem',
         '168': '42rem', 
